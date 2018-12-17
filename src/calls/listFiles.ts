389117a8c28578725f5b6ec8ae9d280e97e45ed3
Chris Tate <chris@zappjs.com>
2018-12-17 05:20:04 -0600

    adds recursive listItems

--- src/calls/listFiles.ts
+++ src/calls/listFiles.ts
@@ -1,24 +1,45 @@
 import * as fs from 'fs-extra';
 import * as path from 'path';
 
-export async function listFiles({ body }: any) {
-  const items = await fs.readdir(body.cwd);
-  const mappedItems = items
-    .map((itemName) => {
-      const itemPath = path.normalize(`${body.cwd}/${itemName}`);
+import { asyncForEach } from '../lib';
 
-      const isDirectory = fs.statSync(itemPath).isDirectory();
+interface IFile {
+  isEmpty: boolean;
+  name: string;
+  type: string;
+}
 
-      return {
-        isEmpty: isDirectory && fs.readdirSync(itemPath).length === 0,
-        name: itemName,
-        type: isDirectory ? 'dir' : 'file'
-      };
+async function getFilesRecursive(dir: string, cwd: string): Promise<IFile[]> {
+  const items = await fs.readdir(dir);
+
+  let mappedItems: IFile[] = [];
+  await asyncForEach(items, async (itemName: string) => {
+    const cwdPath = path.normalize(cwd);
+    const itemPath = path.normalize(`${dir}/${itemName}`);
+
+    const isDirectory = fs.statSync(itemPath).isDirectory();
+
+    mappedItems.push({
+      isEmpty: isDirectory && fs.readdirSync(itemPath).length === 0,
+      name: `${dir.substr(cwdPath.length)}/${itemName}`,
+      type: isDirectory ? 'dir' : 'file'
     });
+
+    if (isDirectory) {
+      const childItems = await getFilesRecursive(itemPath, cwd);
+      mappedItems = mappedItems.concat(childItems);
+    }
+  });
+  
+  return mappedItems;
+}
+
+export async function listFiles({ body }: any) {
+  const items = await getFilesRecursive(body.cwd, body.cwd);
 
   return {
     body: {
-      items: mappedItems
+      items
     }
   };
 }
--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,2 @@
+export * from './asyncForEach';
 export * from './isDirectory';
